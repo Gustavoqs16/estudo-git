157f84c22c65d37d6b89ab56b2d06f79d9f0aeb9
Gustavo <gustavo.souza@zeroti.com.br>
2021-09-16 10:28:28 -0300

    Aula 15 React-udemy

--- src/App.js
+++ src/App.js
@@ -1,46 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
     state = {
-        name: 'Gustavo Queiroz',
-        counter: 0
+        posts: [
+            {
+                id: 1,
+                title: 'Título 1',
+                body: 'Corpo 1'
+            },
+            {
+                id: 2,
+                title: 'Título 2',
+                body: 'Corpo 2'
+            },
+            {
+                id: 3,
+                title: 'Título 3',
+                body: 'Corpo 3'
+            }
+        ]
     };
-
-    handlePClick = () => {
-        this.setState({ name: 'Junior' });
-    }
-
-    handleAClick = (event) => {
-        event.preventDefault();
-        const { counter } = this.state;
-        this.setState({ counter: counter + 1 });
-    }
 
     render() {
         const { name, counter } = this.state;
 
-        return ( <
-            div className = "App" >
-            <
-            header className = "App-header" >
-            <
-            img src = { logo }
-            className = "App-logo"
-            alt = "logo" / >
-            <
-            p onClick = { this.handlePClick } > { name } { counter } <
-            /p> <
-            a onClick = { this.handleAClick }
-            className = "App-link"
-            href = "https://reactjs.org"
-            target = "_blank"
-            rel = "noopener noreferrer" >
-            Learn React <
-            /a> <
-            /header> <
-            /div>
+        return (
+            <div className="App" >
+                {posts.map(post => (
+                    <div key={post.id}>
+                        <h1>{post.title}</h1>
+                        <p>{post.bory}</p>
+                    </div>
+                ))}
+            </div>
         );
     }
 }
