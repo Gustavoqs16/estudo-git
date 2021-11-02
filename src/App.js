cd2080afb1584eb3c69ba3dbca09a0e6d1288f0b
Gustavoqs16 <gustavo_qs16@hotmail.com>
2021-11-01 21:41:52 -0300

    Aula 16 - Lifecycle methods ( Parte 1)

--- src/App.js
+++ src/App.js
@@ -1,49 +1,65 @@
-import './App.css';
-import { Component } from 'react';
+import "./App.css";
+import { Component } from "react";
 
 class App extends Component {
-    state = {
-        posts: []
-    };
+  state = {
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: "Título 1",
+        body: "Corpo 1",
+      },
+      {
+        id: 2,
+        title: "Título 2",
+        body: "Corpo 2",
+      },
+      {
+        id: 3,
+        title: "Título 3",
+        body: "Corpo 3",
+      },
+    ],
+  };
+  timeoutUpdate = null;
 
-    componentDidMount(){
-        setTimeout(() => {
-            this.setState({
-                posts: [
-                    {
-                        id: 1,
-                        title: 'Título 1',
-                        body: 'Corpo 1'
-                    },
-                    {
-                        id: 2,
-                        title: 'Título 2',
-                        body: 'Corpo 2'
-                    },
-                    {
-                        id: 3,
-                        title: 'Título 3',
-                        body: 'Corpo 3'
-                    }
-                ]
-            })
-        }, 5000);
-    }
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = "O titulo mudou";
+    this.handleTimeout = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 3000);
+  };
 
-    render() {
-        const { posts } = this.state;
+  componentDidMount() {
+    this.handleTimeout();
+  }
 
-        return (
-            <div className="App" >
-                {posts.map(post => (
-                    <div key={post.id}>
-                        <h1>{post.title}</h1>
-                        <p>{post.body}</p>
-                    </div>
-                ))}
-            </div>
-        );
-    }
+  componentDidUpdate() {
+    clearTimeout();
+    this.handleTimeout();
+  }
+
+  componentWillUnmount(){
+      clearTimeout(this.timeoutUpdate);
+  }
+
+  render() {
+    const { posts, counter } = this.state;
+
+    return (
+      <div className="App">
+        <h1>{counter}</h1>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
